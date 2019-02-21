461584aabf93d749852c5abc2ebf3d6bb8dddf7a
cara ottmar <cara.ottmar@gmail.com>
2019-02-20 19:16:01 -0800

    tutorial complete

---
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+// REPLACED BY IN-MEMORY-DATA
+
+// import { Hero } from './hero';
+
+// export const HEROES: Hero[] = [
+//   { id: 11, name: 'Billie Jean' },
+//   { id: 12, name: 'Birdie' },
+//   { id: 13, name: 'Jedi' },
+//   { id: 14, name: 'Grace' },
+//   { id: 15, name: 'Lady' },
+//   { id: 16, name: 'Sam' },
+//   { id: 17, name: 'Sassy' },
+//   { id: 18, name: 'Miss Kitty' }
+
+// ]
